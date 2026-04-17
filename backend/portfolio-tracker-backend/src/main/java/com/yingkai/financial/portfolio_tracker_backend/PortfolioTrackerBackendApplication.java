e22e01ad25c1a6c1f4086fb168ed65ac22824790
yingkailee <yingkailee0@gmail.com>
2026-04-16 22:29:38 -0500

    add starter files

---
+++ backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/PortfolioTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yingkai.financial.portfolio_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTrackerBackendApplication.class, args);
+	}
+
+}
