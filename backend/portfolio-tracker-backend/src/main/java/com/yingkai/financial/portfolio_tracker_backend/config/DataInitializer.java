8f13e47af96c9093f8508a00492350278bc4a33e
yingkailee <yingkailee0@gmail.com>
2026-04-21 20:42:33 -0500

    Iterate through FundType enum in DataInitializer

--- backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/config/DataInitializer.java
+++ backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/config/DataInitializer.java
@@ -2,7 +2,6 @@ package com.yingkai.financial.portfolio_tracker_backend.config;
 
 import com.yingkai.financial.portfolio_tracker_backend.entity.Fund;
 import com.yingkai.financial.portfolio_tracker_backend.entity.FundType;
-import java.util.List;
 import com.yingkai.financial.portfolio_tracker_backend.repository.FundRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -19,14 +18,9 @@ public class DataInitializer {
     CommandLineRunner initDatabase() {
         return args -> {
             if (fundRepository.count() == 0) {
-                fundRepository.saveAll(List.of(
-                    new Fund(null, FundType.VOO.getTicker(), FundType.VOO.getName()),
-                    new Fund(null, FundType.VTI.getTicker(), FundType.VTI.getName()),
-                    new Fund(null, FundType.BND.getTicker(), FundType.BND.getName()),
-                    new Fund(null, FundType.VNQ.getTicker(), FundType.VNQ.getName()),
-                    new Fund(null, FundType.QQQ.getTicker(), FundType.QQQ.getName()),
-                    new Fund(null, FundType.VEA.getTicker(), FundType.VEA.getName())
-                ));
+                for (FundType type : FundType.values()) {
+                    fundRepository.save(new Fund(null, type.getTicker(), type.getName()));
+                }
             }
         };
     }
