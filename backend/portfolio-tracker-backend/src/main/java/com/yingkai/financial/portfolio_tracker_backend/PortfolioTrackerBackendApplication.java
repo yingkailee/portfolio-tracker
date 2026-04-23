42118720cabacd8d8feaad1d259dc37e45a78e5b
yingkailee <yingkailee0@gmail.com>
2026-04-23 09:12:44 -0500

    Add DB env debug logging

--- backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/PortfolioTrackerBackendApplication.java
+++ backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/PortfolioTrackerBackendApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PortfolioTrackerBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_USER: " + System.getenv("DB_USER"));
+		System.out.println("DB_PASSWORD: " + (System.getenv("DB_PASSWORD") != null ? "***" : "null"));
 		SpringApplication.run(PortfolioTrackerBackendApplication.class, args);
 	}
 
