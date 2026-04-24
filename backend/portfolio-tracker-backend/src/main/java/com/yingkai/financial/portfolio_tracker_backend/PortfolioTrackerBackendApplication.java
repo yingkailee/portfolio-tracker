a57b5f6902e007310ef18141510ac92550635d04
yingkailee <yingkailee0@gmail.com>
2026-04-23 20:14:18 -0500

    Use ignoreIfMissing for dotenv in production

--- backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/PortfolioTrackerBackendApplication.java
+++ backend/portfolio-tracker-backend/src/main/java/com/yingkai/financial/portfolio_tracker_backend/PortfolioTrackerBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PortfolioTrackerBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		dotenv.entries().forEach(e -> 
 			System.setProperty(e.getKey(), e.getValue())
 		);
